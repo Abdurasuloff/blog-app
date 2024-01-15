de03ffd1b66c00c2644b91e0cbd6eabcd34d3fce
Akbarjon <abdurasuloff2004@gmail.com>
2024-01-15 16:21:56 +0500

    For Deploy

--- core/settings.py
+++ core/settings.py
@@ -11,23 +11,32 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(50jgq_@pclxq24dhdb%a_1px(-d(j=_m96-8tu4wl6coqz2u)'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://7df8-94-158-60-0.ngrok-free.app']
+# CSRF_TRUSTED_ORIGINS = ['https://7df8-94-158-60-0.ngrok-free.app']
 # Application definition
 
 INSTALLED_APPS = [
@@ -137,6 +146,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR.joinpath("media")
